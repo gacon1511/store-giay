dcb81cc92abfd26ebf6e39e5ad6df70b9df29c72
gacon1511 <hoaiphuc15987@gmail.com>
2021-06-26 19:27:46 +0700

    Initial commit

---
+++ store giay/Controllers/GiayStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace store_giay.Controllers
+{
+    public class GiayStoreController : Controller
+    {
+        // GET: GiayStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
