ec3e1d2ef8e4c8136b0b1c38f2be57a94d8390e5
Anh Duy <anhduyg07@gmail.com>
2021-06-28 02:39:15 +0700

    Xong lab02 (còn lỗi hiển thị sản phẩm mới)

--- store giay/Controllers/GiayStoreController.cs
+++ store giay/Controllers/GiayStoreController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using store_giay.Models;
 
 namespace store_giay.Controllers
 {
     public class GiayStoreController : Controller
     {
         // GET: GiayStore
+
+        dbStoregiayDataContext data = new dbStoregiayDataContext();
+
+        private List<Sanpham> Laygiaymoi(int count)
+        {
+            return data.Sanphams.OrderByDescending(a => a.ngaycapnhat).Take(count).ToList();
+        }
         public ActionResult Index()
         {
-            return View();
+            var giaymoi = Laygiaymoi(5);
+            return View(giaymoi);
+        }
+        public ActionResult Loaisanpham()
+        {
+            var loaisanpham = from lsp in data.Loaisanphams select lsp;
+            return PartialView(loaisanpham);
+        }
+        public ActionResult Nhanhieu()
+        {
+            var nhanhieu = from nh in data.Nhanhieus select nh;
+            return PartialView(nhanhieu);
         }
     }
 }
