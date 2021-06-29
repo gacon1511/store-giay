1be310345e4a426d2ca5675fac01ace3ee26e87d
Anh Duy <anhduyg07@gmail.com>
2021-06-30 01:48:01 +0700

    Xong lab06

--- store giay/App_Start/RouteConfig.cs
+++ store giay/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace store_giay
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "GiayStore", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
